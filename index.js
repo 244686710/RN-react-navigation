fc482016027e43f72745959426198515ba76cba9
yu.yadong <yuyadong@zgrj.net>
2019-02-21 16:37:07 +0800

    tab-navigation, react-native-vector-icons, tab切换

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import TabNavigator from './TabNavigationDemo'; // 测试 TabNavigator
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TabNavigator);
